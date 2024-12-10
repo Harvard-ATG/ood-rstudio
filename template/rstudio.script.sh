58dc2409e5daf5f3459353aced134e0cab33d77c
Jeremy Guillette <3651377+jaguillette@users.noreply.github.com>
2024-12-10 10:42:51 -0500

    update template to make things work

--- template/rstudio.script.sh
+++ template/rstudio.script.sh
@@ -2,7 +2,7 @@
 
 set -x
 
-echo "rstudio.script.sh is running"
+export PATH=/usr/lib/rstudio-server/bin:$PATH
 
 RSERVER_ARGLIST=(
   "--server-working-dir" "${WORKING_DIR}"
@@ -12,8 +12,7 @@ RSERVER_ARGLIST=(
   "--www-port" "${port}"
   "--rsession-path" "${RSESSION_WRAPPER_FILE}"
   "--auth-none" "0"
-  "--auth-pam-helper-path" "pam-helper"
-  # "--auth-pam-helper-path" "${RSTUDIO_AUTH}"
+  "--auth-pam-helper-path" "${RSTUDIO_AUTH}"
   "--auth-encrypt-password" "0"
 )
 echo "RSERVER_ARGLIST=${RSERVER_ARGLIST[@]}"
@@ -22,8 +21,9 @@ RSERVER_ARGS="${RSERVER_ARGLIST[@]}"
 
 echo "which rserver=$(which rserver)"
 
-sh -c "USER=${USER}
-rserver ${RSERVER_ARGS}"
+rserver ${RSERVER_ARGS}
+
+echo "$?"
 
 echo "$(ps aux | grep rserver)"
 
